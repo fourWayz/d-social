b2654c69c3b5778f421f1daf332f4acab8ed7711
fourWayz <157867069+fourWayz@users.noreply.github.com>
2024-01-28 11:12:22 +0000

    add more tests

--- test/social.js
+++ test/social.js
@@ -52,6 +52,48 @@ describe('SocialMedia Contract', function () {
     expect(events[0].args.content).to.equal(content);
   });
 
+  it('should like a post', async function () {
+    const content = 'This is a test post';
+
+    await socialMedia.connect(user1).registerUser('user1');
+    await socialMedia.connect(user1).createPost(content);
+    await socialMedia.connect(user2).registerUser('user2');
+
+    await socialMedia.connect(user2).likePost(0);
+
+    const post = await socialMedia.getPost(0);
+    expect(post.likes).to.equal(1);
+
+    const events = await socialMedia.queryFilter('PostLiked');
+    expect(events.length).to.equal(1);
+    expect(events[0].args.liker).to.equal(user2.address);
+    expect(events[0].args.postId).to.equal(0);
+  });
+
+  it('should add a comment to a post', async function () {
+    const content = 'This is a test post';
+    const commentContent = 'This is a test comment';
+
+    await socialMedia.connect(user1).registerUser('user1');
+    await socialMedia.connect(user1).createPost(content);
+    await socialMedia.connect(user2).registerUser('user2');
+
+    await socialMedia.connect(user2).addComment(0, commentContent);
+
+    const comment = await socialMedia.getComment(0, 0);
+    expect(comment.commenter).to.equal(user2.address);
+    expect(comment.content).to.equal(commentContent);
+
+    const post = await socialMedia.getPost(0);
+    expect(post.commentsCount).to.equal(1);
+
+    const events = await socialMedia.queryFilter('CommentAdded');
+    expect(events.length).to.equal(1);
+    expect(events[0].args.commenter).to.equal(user2.address);
+    expect(events[0].args.postId).to.equal(0);
+    expect(events[0].args.content).to.equal(commentContent);
+  });
+
   // Add more tests for other functions
 
 });
